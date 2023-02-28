44795e9b5ea0794f5a1701beda5a66c5f5a1e9ec
Karlo2001 <karl.bilsted.michelsen@gmail.com>
2023-02-28 14:48:16 +0100

    Update run script to support skipping creating a database, and adding result json files in appropriate folder with appropriate name

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-if [ ! -d "database-$1}" ]
+if [ ! "$2" = "skipDatabase" ]
 then
-  codeql database create codeql-database-$1 --language=$1 --overwrite --threads=0
-else
-  codeql database create codeql-database-$1 --language=$1
+  if [ ! -d "database-$1}" ]
+  then
+    codeql database create codeql-database-$1 --language=$1 --overwrite --threads=0
+  else
+    codeql database create codeql-database-$1 --language=$1
+  fi
 fi
 codeql database run-queries codeql-database-$1
 echo "Interpreting results"
@@ -11,9 +14,10 @@ datapath2=$dir\\codeql-database-$1\\results\\codeql\\java
 k=1
 for i in "$datapath2"/*.bqrs
 do
-  codeql bqrs decode "$i" -o=test$k.json --format=json
+  path=$(cut -d "." -f 1 <<< "$i")
+  queryFile=$(echo ${path##*/})
+  codeql bqrs decode "$i" -o=results\\$queryFile.json --format=json
   k=$(($k + 1))
 done
-cd ..\\..\\..\\..\\..\\..
 npm start
 $SHELL
