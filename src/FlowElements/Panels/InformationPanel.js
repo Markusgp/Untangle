eb37d285bf6b2287a2e77f4aebc4747673d78bcb
Gustav Müller Christoffersen <gmcblaxk@gmail.com>
2023-04-06 11:53:54 +0200

    Added buttons for "hide" & "expand" to informationpanel

--- src/FlowElements/Panels/InformationPanel.js
+++ src/FlowElements/Panels/InformationPanel.js
@@ -2,10 +2,13 @@ import React, {useState} from 'react';
 import "./PanelStyles.css"
 import ObjectLogo from "../Assets/Object.svg"
 import Drop from "../Assets/Drop.svg"
-
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import { CgExpand } from "react-icons/cg";
 
 const InformationPanel = (props) => {
     const toggle = () => setOpen(!open);
+
+    const isPackage = props.type === "package";
 
     const [open, setOpen] = useState(true);
 
@@ -31,23 +34,30 @@ const InformationPanel = (props) => {
                         <div className="listElementHolder">
                             <div className="leftFloat">
                                 <span className="dot"></span>
+                                <p><strong>Type:</strong></p>
+                            </div>
+                            <p><i>{props.type.charAt(0).toUpperCase() + props.type.slice(1)}</i></p>
+                        </div>
+                        <div className="listElementHolder">
+                            <div className="leftFloat">
+                                <span className="dot"></span>
                                 <p><strong>Name:</strong></p>
                             </div>
-                            <p>{props.name}</p>
+                            <p><i>{props.name}</i></p>
                         </div>
                         <div className="listElementHolder">
                             <div className="leftFloat">
                                 <span className="dot"></span>
                                 <p><strong>Q-Name:</strong></p>
                             </div>
-                            <p>{props.pack}</p>
+                            <p><i>{props.pack}</i></p>
                         </div>
                         <div className="listElementHolder">
                             <div className="leftFloat">
                                 <span className="dot"></span>
                                 <p><strong>Hidden:</strong></p>
                             </div>
-                            <p>{(!props.visible).toString()}</p>
+                            <p><i>{(!props.visible).toString()}</i></p>
                         </div>
                         {props.linesOfCode != null && (<div className="listElementHolder">
                             <div className="leftFloat">
@@ -55,8 +65,20 @@ const InformationPanel = (props) => {
                                 <p><strong>Lines of Code:</strong></p>
                             </div>
 
-                            <p>{props.linesOfCode.toString()}</p>
+                            <p><i>{props.linesOfCode.toString()}</i></p>
                         </div>)}
+                    </div>
+                    <div className="buttonWrapper">
+                        {isPackage && (
+                          <div className="iconButton" id="expand">
+                              <p>Expand</p>
+                              <CgExpand className="ExpandIcon"></CgExpand>
+                          </div>
+                        )}
+                        <div className="iconButton" id="hide">
+                            <p>Hide</p>
+                            <FaEye className="EyeIcon"></FaEye>
+                        </div>
                     </div>
                 </div>
             )}
