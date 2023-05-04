73e59265b8ffb0d6d6cda446070c5c3f6c4de3ca
Karlo2001 <karl.bilsted.michelsen@gmail.com>
2023-05-04 18:30:50 +0200

    Added check for arguments

--- untangle.sh
+++ untangle.sh
@@ -1,3 +1,9 @@
+if [ ! "$#" -eq "2" ]; then
+  echo "You should only pass exactly 2 arguments, first the projects directory and then the java version"
+  echo "Press enter to exit"
+  read user_read
+  exit 1
+fi
 docker pull karlo2001/untangle:latest
 docker run -d -it -p 8080:3000 --name untangled karlo2001/untangle
 cd $1
