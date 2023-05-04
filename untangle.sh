7a0c397ed476c38bf566d2df66fde4646cb028bb
Karlo2001 <kabm@itu.dk>
2023-05-04 21:45:01 +0200

    Changed run script to make it more consistent with the ps script

--- untangle.sh
+++ untangle.sh
@@ -1,12 +1,11 @@
-if [ ! "$#" -eq "2" ]; then
-  echo "You should only pass exactly 2 arguments, first the projects directory and then the java version"
+if [ ! "$#" -eq "1" ]; then
+  echo "You should only pass exactly 1 arguments, which is the java version"
   echo "Press enter to exit"
   read user_read
   exit 1
 fi
 docker pull karlo2001/untangle:latest
 docker run -d -it -p 8080:3000 --name untangled karlo2001/untangle
-cd $1
 docker cp . untangled:/var/www
-docker exec untangled //bin/bash -c "./docker-run.sh $2"
+docker exec untangled //bin/bash -c "./docker-run.sh $1"
 $SHELL
