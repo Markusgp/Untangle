270e66ef321c9d4d2b7a7a328db89255854338cd
Karlo2001 <kabm@itu.dk>
2023-05-04 21:30:05 +0200

    Try no params in ps1

--- untangle.ps1
+++ untangle.ps1
@@ -1,7 +1,4 @@
-param ([Parameter(Mandatory)]$path, [Parameter(Mandatory)]$java_version)
-
 docker pull karlo2001/untangle:latest
 docker run -d -it -p 8080:3000 --name untangled karlo2001/untangle
-cd $path
 docker cp . untangled:/var/www
-docker exec untangled //bin/bash -c "./docker-run.sh $java_version"
+docker exec untangled //bin/bash -c "./docker-run.sh $Env:UNTANGLE_JAVA"
