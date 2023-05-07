55810bfc72293b82707a3ee639628eb6821441fd
Karlo2001 <kabm@itu.dk>
2023-05-07 22:25:10 +0200

    Add script that allocates a lot of memory for the container

---
+++ untangle8gb.sh
@@ -0,0 +1,11 @@
+if [ ! "$#" -eq "1" ]; then
+  echo "You should only pass exactly 1 arguments, which is the java version"
+  echo "Press enter to exit"
+  read user_read
+  exit 1
+fi
+docker pull karlo2001/untangle:latest
+docker run -d -it --memory="8g" -p 8080:3000 --name untangled karlo2001/untangle
+docker cp . untangled:/var/www
+docker exec untangled //bin/bash -c "./docker-run.sh $1"
+$SHELL
