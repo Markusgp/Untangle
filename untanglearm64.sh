7c46353f7f76f981fbb8a34f9a6604aecf9ca82a
Karlo2001 <kabm@itu.dk>
2023-05-07 16:48:39 +0200

    Add arm64 support

---
+++ untanglearm64.sh
@@ -0,0 +1,11 @@
+if [ ! "$#" -eq "1" ]; then
+  echo "You should only pass exactly 1 arguments, which is the java version"
+  echo "Press enter to exit"
+  read user_read
+  exit 1
+fi
+docker pull karlo2001/untangle:arm64
+docker run -d -it -p 8080:3000 --name untangled karlo2001/untangle
+docker cp . untangled:/var/www
+docker exec untangled //bin/bash -c "./docker-run.sh $1"
+$SHELL
